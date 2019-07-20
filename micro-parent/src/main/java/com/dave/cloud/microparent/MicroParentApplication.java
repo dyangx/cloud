0d117b77e6cb3ce15a11d1956e421bafd6c2f209
dyangx <yaabbccj@163.com>
2019-07-20 11:01:29 +0800

    init

---
+++ micro-parent/src/main/java/com/dave/cloud/microparent/MicroParentApplication.java
@@ -0,0 +1,13 @@
+package com.dave.cloud.microparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroParentApplication.class, args);
+	}
+
+}
